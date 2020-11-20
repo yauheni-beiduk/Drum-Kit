93bdf22e0ef0325d59f98594014f5cc6656d6cd7
yauheni-beiduk <yauheni.beiduk@gmail.com>
2020-11-20 15:45:09 +0300

    fix:add click  js

--- main.js
+++ main.js
@@ -15,4 +15,41 @@ function removeTransition(e) {
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition)); // Back to beginner mode
 
+
+const addAudioListener = (buttonEl, name) => {
+    buttonEl.addEventListener('click', function() {
+        const audio = document.querySelector(`audio[data-key="${name}"]`);
+        if(!audio) return;      
+        audio.currentTime = 0;  
+        audio.play();
+        buttonEl.classList.add('playing');
+})};
+
+
+const q = document.querySelector(`div[data-key="q"]`);
+const w = document.querySelector(`div[data-key="w"]`);
+const e = document.querySelector(`div[data-key="e"]`);
+const r = document.querySelector(`div[data-key="r"]`);
+const t = document.querySelector(`div[data-key="t"]`);
+const y = document.querySelector(`div[data-key="y"]`);
+const u = document.querySelector(`div[data-key="u"]`);
+const i = document.querySelector(`div[data-key="i"]`);
+const o = document.querySelector(`div[data-key="o"]`);
+const p = document.querySelector(`div[data-key="p"]`);
+
+
+addAudioListener(q , "q");
+addAudioListener(w , "w");
+addAudioListener(e , "e");
+addAudioListener(r , "r");
+addAudioListener(t , "t");
+addAudioListener(y , "y");
+addAudioListener(u , "u");
+addAudioListener(i , "i");
+addAudioListener(o , "o");
+addAudioListener(p , "p");
+
 window.addEventListener('keydown', playSound);
+
+
+
